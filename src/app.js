7c166e1eff07149d149a7134b076e576fb1a5663
antal-komornik <komornik.anti@mail.com>
2022-07-27 15:53:59 +0200

    Új külső és belső

--- src/app.js
+++ src/app.js
@@ -1,91 +1,106 @@
-// TODO 
-// kocka húzás => kirakás, pontbeírása
-// lövés, futás kezelése
-// gombok kezelése
-//  pulled [] ot átalakítani ugy hogy a lábakat beleszamitsa
+/*  TODO 
+    újrahúzáskijavítása
+    stay gomb
+    stat kijelzése
+*/
 
 
-// a zsákban 13 kocka van, 3 szinben, 6 zöld,4 sárga, 3 piros
-// a bag tömböt feltöltöm véletlenszerűen
-let bag = ["G", "G", "G", "Y", "Y", "G", "R", "Y", "Y", "R", "G", "G", "R"];
+
+// létrehozom a kockák tulajdonságaikat
+// a három színű kockák darabszáma
+let green = 6;
+let yellow = 4;
+let red = 3;
 const colors = ["G", "Y", "R"];
+// az egyes kockákok megtalálható szimbólumok
+const g = ["B", "B", "B", "R", "R", "S"];
+const y = ["B", "B", "R", "R", "S", "S"];
+const r = ["B", "R", "R", "S", "S", "S"];
+// ez mutatja meg, hogy hány darab kocka van kihúzva
+let dices = 0;
 
-// az egyes színeken, milyen szimGólumok vannak.
-const green = ["B", "B", "B", "R", "R", "S"];
-const yellow = ["B", "B", "R", "R", "S", "S"];
-const red = ["B", "R", "R", "S", "S", "S"];
-let symbol = "";
-let pulled_dice = 0;
+// ez mutatja meg hogy, mik lettek kihúzva
 let pulled = [];
 
-
-
 function pullDice() {
-  // szinek alapján, melyik szmbólum legyen
-  let k = Math.floor(Math.random() * 6);
-  // ez a változó adja meg hogy melyik szín legyen
-  let n = Math.floor(Math.random() * 13);
-  let img_dice = document.createElement("img");
-  img_dice.setAttribute("id", "imgDice");
+  // létrehozom a képet és elérési helyét 
+  let imgDice = document.createElement("img");
+  imgDice.setAttribute("id", "diceID");
   let card = "";
 
-  if (bag[n] == "G") {
-    card = bag[n] + "-" + green[k];
-    pulled.push(bag[n]);
-    bag.splice(n, 1);
-  }
-  else if (bag[n] == "Y") {
-    card = bag[n] + "-" + yellow[k];
-    pulled.push(bag[n]);
-    bag.splice(n, 1);
-  }
-  else if (bag[n] == "R") {
-    card = bag[n] + "-" + red[k];
-    pulled.push(bag[n]);
-    bag.splice(n, 1);
+  // a színek és szímbólumok random változója
+  let n = Math.floor(Math.random() * 3);
+  let k = Math.floor(Math.random() * 6);
+
+  // random kiválasztom a kockákat, addig amíg a kockák száma el nem éri a hármat
+  if (dices < 3) {
+    if (colors[n] == "G" && green != 0) {
+      dices += 1;
+      green -= 1;
+      card = colors[n] + "-" + g[k];
+      pulled = card;
+    }
+    else if (colors[n] == "Y" && y != 0) {
+      dices += 1;
+      yellow -= 1;
+      card = colors[n] + "-" + y[k];
+      pulled = card;
+    }
+    else if (colors[n] == "R" && red != 0) {
+      dices += 1;
+      red -= 1;
+      card = colors[n] + "-" + r[k];
+      pulled = card;
+    }
+    else {
+      console.log("valami hiba");
+      return;
+    }
   }
   else {
+    removeDice();
+  }
+
+  imgDice.src = "../img/dices/" + card + ".jpg";
+
+  const parent = document.getElementById("dice-img");
+  parent.appendChild(imgDice);
+
+  console.log(green);
+  console.log(yellow);
+  console.log(red);
+  console.log("a kockák száma " + dices);
+}
+
+function pushLuck() {
+  pullDice();
+  // ha lenne üres img akkor ez letörlni
+  let err = "http://127.0.0.1:5500/img/dices/.jpg";
+  let e = document.getElementById("diceID").src;
+
+  if (e == err) {
+    removeDice();
+  } else {
     return;
   }
-  console.log(card);
-  img_dice.src = "../img/dices/" + card + ".jpg";
-  document.getElementById("card-container").appendChild(img_dice);
 }
 
-function hitLuck() {
-  for (let i = 0; i < 4; i++) {
-    pullDice();
+function removeDice() {
+  // kitörlöm a kockákat a tábláról
+  const parentElement = document.querySelector("#dice-img");
+  // let allChildren = parentElement.querySelectorAll(":scope > img");
+  let allChildren = parentElement.querySelectorAll(" img");
+
+  // végigmegyek az összes képen
+  allChildren.forEach((item) => item.parentNode.removeChild(item));
+
+  // lenullázom a kockák számát a táblán
+  dices = 0;
+}
+
+function updateHTML(elmId, value) {
+  var elem = document.getElementById(elmId);
+  if (typeof elem !== 'undefined' && elem !== null) {
+    elem.src = value;
   }
-  console.log(bag);
-  console.log(pulled);
 }
-
-// function Stay() {
-//   const delete = document.getElementById("imgDice");
-//   delete.remove();
-// }
-
-function writeStat() {
-
-}
-
-function gameRule() {
-
-}
-
-function updateHand() {
-
-}
-
-// let bag = ["G", "G", "G", "G", "G", "G", "Y", "Y", "Y", "Y", "R", "R", "R"];
-// let n = Math.floor(Math.random() * 13);
-// let pulled = [];
-
-// console.log(n);
-
-// function hitLuck() {
-//   pulled.push(bag[n]);
-//   bag.splice(n, 1);
-//   console.log(bag);
-//   console.log(pulled);
-// }
